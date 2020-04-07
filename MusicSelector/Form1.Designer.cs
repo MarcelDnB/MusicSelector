b1fa5ed0df8be0c9cecd87173a27efcec5d0175b
Marcel Dan <gigi.dan2011@gmail.com>
2020-04-07 22:24:21 +0200

    Initial commit

---
+++ MusicSelector/Form1.Designer.cs
@@ -0,0 +1,229 @@
+﻿namespace MusicSelector
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openFolderToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.keepFolderToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.debugBox = new System.Windows.Forms.TextBox();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.Font = new System.Drawing.Font("Arial", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 22;
+            this.listBox1.Location = new System.Drawing.Point(161, 174);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.ScrollAlwaysVisible = true;
+            this.listBox1.Size = new System.Drawing.Size(756, 356);
+            this.listBox1.TabIndex = 1;
+            this.listBox1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.listBox1_MouseClick);
+            this.listBox1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.listBox1_MouseDoubleClick);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(161, 50);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(103, 48);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Next";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(813, 50);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(104, 48);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Keep";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1085, 24);
+            this.menuStrip1.TabIndex = 4;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openFolderToolStripMenuItem,
+            this.keepFolderToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // openFolderToolStripMenuItem
+            // 
+            this.openFolderToolStripMenuItem.Name = "openFolderToolStripMenuItem";
+            this.openFolderToolStripMenuItem.Size = new System.Drawing.Size(139, 22);
+            this.openFolderToolStripMenuItem.Text = "Open Folder";
+            this.openFolderToolStripMenuItem.Click += new System.EventHandler(this.openFolder_Click);
+            // 
+            // keepFolderToolStripMenuItem
+            // 
+            this.keepFolderToolStripMenuItem.Name = "keepFolderToolStripMenuItem";
+            this.keepFolderToolStripMenuItem.Size = new System.Drawing.Size(139, 22);
+            this.keepFolderToolStripMenuItem.Text = "Keep Folder";
+            this.keepFolderToolStripMenuItem.Click += new System.EventHandler(this.keepFolder_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 174);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Mode: ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(58, 174);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(71, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Album/Songs";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(90, 207);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(15, 14);
+            this.checkBox1.TabIndex = 7;
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 207);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(72, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Same Folder: ";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(474, 50);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(103, 48);
+            this.button3.TabIndex = 9;
+            this.button3.Text = "Play/Stop";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(161, 145);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 10;
+            this.button4.Text = "Back";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // debugBox
+            // 
+            this.debugBox.Location = new System.Drawing.Point(923, 174);
+            this.debugBox.Multiline = true;
+            this.debugBox.Name = "debugBox";
+            this.debugBox.Size = new System.Drawing.Size(150, 306);
+            this.debugBox.TabIndex = 12;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1085, 554);
+            this.Controls.Add(this.debugBox);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openFolderToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem keepFolderToolStripMenuItem;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.TextBox debugBox;
+    }
+}
+
---
+++ MusicSelector/Form1.cs
@@ -0,0 +1,232 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using WMPLib;
+using System.Threading;
+using System.Collections;
+namespace MusicSelector
+{
+    
+    public partial class Form1 : Form
+    {
+        private List<String> dirs = new List<String>();
+        private FolderBrowserDialog FBDOpen = new FolderBrowserDialog();
+        private WMPLib.WindowsMediaPlayer wplayer = new WMPLib.WindowsMediaPlayer();
+        private bool switchPlayback = false;
+        private int MouseIndex;
+        private Stack<String> fullRoute = new Stack<String>();
+        private FolderBrowserDialog FBDKeep = new FolderBrowserDialog();
+        private String KeepSelectedPath;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void openFolder_Click(object sender, EventArgs e)
+        {
+
+            FBDOpen = new FolderBrowserDialog();
+            fullRoute.Push(FBDOpen.SelectedPath);
+            if (FBDOpen.ShowDialog() == DialogResult.OK)
+            {
+                listBox1.Items.Clear();
+                dirs = new List<string>(Directory.GetDirectories(FBDOpen.SelectedPath));
+
+                foreach (String dir in dirs)
+                {
+                    listBox1.Items.Add(Path.GetFileName(dir));
+                }
+            }
+
+        }
+
+        private void listBox1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            /*
+             * Para navegar entre los directorios.
+             * 
+             */
+            MouseIndex = this.listBox1.IndexFromPoint(e.Location);
+            String path = dirs[MouseIndex];
+
+            List<String> dirNuevo = new List<String>(Directory.GetDirectories(path));
+
+            listBox1.Items.Clear();
+            foreach (String dir in dirNuevo)
+            {
+                listBox1.Items.Add(Path.GetFileName(dir));
+            }
+
+            String spl = path;
+            List<String> splited = new List<string>(spl.Split('\\'));
+            String finalPrevPath = splited[0];
+            if (splited.Count == 1) {
+                finalPrevPath = finalPrevPath + "\\";
+            }
+
+            for (int i = 1; i < splited.Count - 1; i++) {
+                finalPrevPath = finalPrevPath + "\\" + splited[i];
+            }
+            fullRoute.Push(finalPrevPath + "\\");
+
+
+            if (fullRoute.Count == 1)
+            {
+                button4.Enabled = false;
+            }
+            else
+            {
+                button4.Enabled = true;
+            }
+            /**************************************************************/
+            dirs = dirNuevo;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (wplayer.currentMedia == null)
+            {
+                playBack();
+            }
+            else if (wplayer.currentMedia.sourceURL.Contains(dirs[listBox1.SelectedIndex]))
+            {
+                switchPlayback = !switchPlayback;
+                if (switchPlayback == true)
+                {
+                    wplayer.controls.pause();
+                }
+                else
+                {
+                    wplayer.controls.play();
+                }
+            }
+            else {
+                wplayer.controls.stop();
+                playBack();
+            }
+
+        }
+
+        private void playBack()
+        {
+            String path = dirs[listBox1.SelectedIndex];
+            List<String> songs = new List<String>(Directory.GetFiles(path));
+            wplayer = new WMPLib.WindowsMediaPlayer();
+            WMPLib.IWMPMedia wt;
+            foreach (String song in songs)
+            {
+                if (song[song.Length-1] == '3' && song[song.Length - 2] == 'p' && song[song.Length - 3] == 'm') {
+                    wt = wplayer.newMedia(song);
+                    wplayer.currentPlaylist.appendItem(wt);
+                }
+            }
+            wplayer.controls.play();
+        }
+
+        private void listBox1_MouseClick(object sender, MouseEventArgs e)
+        {
+            MouseIndex = this.listBox1.IndexFromPoint(e.Location);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            String path = fullRoute.Pop();
+            listBox1.Items.Clear();
+           
+            List<String> dirNuevo = new List<String>(Directory.GetDirectories(path));
+            foreach (String dir in dirNuevo)
+            {
+                listBox1.Items.Add(Path.GetFileName(dir));
+            }
+            dirs = dirNuevo;
+
+            if (fullRoute.Count == 1)
+            {
+                button4.Enabled = false;
+            }
+            else
+            {
+                button4.Enabled = true;
+            }
+        }
+
+        private void keepFolder_Click(object sender, EventArgs e)
+        {
+            FBDKeep = new FolderBrowserDialog();
+            if (FBDKeep.ShowDialog() == DialogResult.OK)
+            {
+                KeepSelectedPath = FBDKeep.SelectedPath;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            String path = dirs[MouseIndex];
+            MessageBox.Show("Origen: "+ path+"\n"+"Destino: " + KeepSelectedPath + "\\" + Path.GetDirectoryName(path));
+            DirectoryCopy(path, KeepSelectedPath + "\\" + new DirectoryInfo(path).Name, true);
+        }
+
+
+
+
+
+
+
+
+
+        public static void DirectoryCopy(string sourceDirName, string destDirName, bool copySubDirs)
+        {
+            // Get the subdirectories for the specified directory.
+            DirectoryInfo dir = new DirectoryInfo(sourceDirName);
+
+            if (!dir.Exists)
+            {
+                throw new DirectoryNotFoundException(
+                    "Source directory does not exist or could not be found: "
+                    + sourceDirName);
+            }
+
+            DirectoryInfo[] dirs = dir.GetDirectories();
+            // If the destination directory doesn't exist, create it.
+            if (!Directory.Exists(destDirName))
+            {
+                Directory.CreateDirectory(destDirName);
+            }
+
+            // Get the files in the directory and copy them to the new location.
+            FileInfo[] files = dir.GetFiles();
+            foreach (FileInfo file in files)
+            {
+                string temppath = Path.Combine(destDirName, file.Name);
+                file.CopyTo(temppath, false);
+            }
+
+            // If copying subdirectories, copy them and their contents to new location.
+            if (copySubDirs)
+            {
+                foreach (DirectoryInfo subdir in dirs)
+                {
+                    string temppath = Path.Combine(destDirName, subdir.Name);
+                    DirectoryCopy(subdir.FullName, temppath, copySubDirs);
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            listBox1.SelectedIndex = listBox1.SelectedIndex + 1;
+            wplayer.controls.stop();
+            playBack();
+        }
+    }
+}
